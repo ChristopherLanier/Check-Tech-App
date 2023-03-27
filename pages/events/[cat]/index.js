6c1bf182ef616f9e547fc1fa11e0e4799f4bf841
crlanier22 <crlanier22@gmail.com>
2023-03-26 20:55:41 -0700

    Created dynamic page routing, json file for data, pathing for hyperlinks and function to handle data props

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const EventsCatPage = () => {
+  return (
+    <div>
+    <h1>Events in Ohio</h1>
+        <div>
+            <a href="/event/event1">Event 1</a>
+            <a href="/event/event2">Event 2</a>
+            <a href="/event/event3">Event 3</a>
+            <a href="/event/event4">Event 4</a>
+            <a href="/event/event5">Event 5</a>
+            <a href="/event/event6">Event 6</a> 
+        </div>
+    </div>
+  )
+}
+
+export default EventsCatPage;
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const Page = () => {
+const EventsPage = () => {
     return (
+        <div>
         <h1>Events Page</h1>
+        <a href="">
+          <img />
+          <h2> Events in Ohio</h2>
+        </a>
+        
+        <a href="">
+          <img />
+          <h2> Events in Dubai</h2>
+        </a>
+         
+        <a href="">
+          <img />
+          <h2> Events in London</h2>
+        </a>
+        </div>
     )
 }
 
-export default Page;
+export default EventsPage;
