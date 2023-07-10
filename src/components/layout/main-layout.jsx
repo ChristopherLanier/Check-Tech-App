ddc8e9c1feb0d2e3dde0283b7f3dae54f51cb45e
crlanier22 <crlanier22@gmail.com>
2023-07-09 23:30:32 -0700

    created component in top of app (app.js) that styles pages with header and footer included

---
+++ src/components/layout/main-layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Header } from '../header/header'
+import { Footer } from '../footer/footer'
+
+// Key understanding!! Our pages are dynamic with only the header and footer remaining constant.
+// How do you style or code dynamic pages then? See below
+// All of our pages in the browser will possess both the header and footer opposed to only the home page possessing it  before
+
+const MainLayout = ({children}) => {
+  return (
+    <>
+        <Header/>
+            {children}
+        <Footer/>
+    </>
+   
+  )
+}
+
+export default MainLayout;
